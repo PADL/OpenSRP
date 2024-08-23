2dfa202bb79dec5e501643954aa2ef654fc437df
Luke Howard <lukeh@padl.com>
2024-08-23 17:23:42 +1000

    Revert "forward LLDP frames"
    This reverts commit 8416df0261962ea9198634aa7a83c946768bc4a8.

--- config-vlan.sh
+++ config-vlan.sh
@@ -10,8 +10,6 @@ ip link set dev br0.51 type vlan mvrp on
 nft add table bridge nat
 nft add chain bridge nat PREROUTING { type filter hook prerouting priority dstnat\; policy accept\; }
 
-echo 16384 > /sys/class/net/br0/bridge/group_fwd_mask
-
 # note we don't need to drop LLDP because it's not forwarded by the bridge,
 # whereas the MMRP/MVRP multicast address is (we need to intercept it)
 #nft add rule bridge nat PREROUTING meta ibrname br0 ether daddr { 01:80:c2:00:00:21, 01:80:c2:00:00:0e } log group 10 drop
