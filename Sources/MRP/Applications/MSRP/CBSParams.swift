4fe3d41892739dd4fedc869a798edb911ad78400
Luke Howard <lukeh@padl.com>
2024-09-02 08:52:24 +0000

    add PADL (c) to CBSParams.swift

--- Sources/MRP/Applications/MSRP/CBSParams.swift
+++ Sources/MRP/Applications/MSRP/CBSParams.swift
@@ -1,5 +1,6 @@
 //
 // Copyright (c) 2019, Intel Corporation
+// Portions Copyright (c) 2024, PADL Software Pty Ltd
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
