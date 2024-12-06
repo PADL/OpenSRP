7bfde63edfaa537c349c97b551095f2a083c78ff
Luke Howard <lukeh@padl.com>
2024-12-06 17:38:12 +1100

    configure Qdiscs on lan0...3

--- config-srp.sh
+++ config-srp.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 BR=br0
-ETH_0=enp8s0
-ETH_1=ens4
 HANDLE=9000
 
 set -e
@@ -22,41 +20,29 @@ nft add chain bridge nat PREROUTING { type filter hook prerouting priority dstna
 nft add rule bridge nat PREROUTING meta ibrname ${BR} ether daddr 01:80:c2:00:00:21 log group 10 drop
 nft list ruleset
 
+echo ""
+
 # https://tsn.readthedocs.io/qdiscs.html
 
-echo ""
-echo "Configuring ${ETH_0} qdisc..."
+for INDEX in 0 1 2 3
+do
+	ETH="lan${INDEX}"
+	echo "Configuring ${ETH} qdisc..."
 
-tc qdisc del dev ${ETH_0} parent root handle ${HANDLE} || : >/dev/null 2>&1
-tc qdisc add dev ${ETH_0} parent root handle ${HANDLE} mqprio \
-	num_tc 3 \
-	map 2 2 1 0 2 2 2 2 2 2 2 2 2 2 2 2 \
-	queues 1@0 1@1 2@2 \
-	hw 0
-tc qdisc replace dev ${ETH_0} parent ${HANDLE}:1 cbs \
-	idleslope 98688 sendslope -901312 hicredit 153 locredit -1389 \
-	offload 1
-tc qdisc replace dev ${ETH_0} parent ${HANDLE}:2 cbs \
-        idleslope 3648 sendslope -996352 hicredit 12 locredit -113 \
-        offload 1
+	tc qdisc del dev ${ETH} parent root handle ${HANDLE} || : >/dev/null 2>&1
+	tc qdisc add dev ${ETH} parent root handle ${HANDLE} mqprio \
+		num_tc 3 \
+		map 2 2 1 0 2 2 2 2 2 2 2 2 2 2 2 2 \
+		queues 1@3 1@2 2@0 \
+		hw 1
+	tc qdisc replace dev ${ETH} parent ${HANDLE}:1 cbs \
+		idleslope 98688 sendslope -901312 hicredit 153 locredit -1389 \
+		offload 1
+	tc qdisc replace dev ${ETH} parent ${HANDLE}:2 cbs \
+		idleslope 3648 sendslope -996352 hicredit 12 locredit -113 \
+		offload 1
 
-echo "${ETH_0} configured!\n"
-
-echo "Configuring ${ETH_1} qdisc..."
-
-tc qdisc del dev ${ETH_1} parent root handle ${HANDLE} || : >/dev/null 2>&1
-tc qdisc add dev ${ETH_1} parent root handle ${HANDLE} mqprio \
-	num_tc 3 \
-	map 2 2 1 0 2 2 2 2 2 2 2 2 2 2 2 2 \
-	queues 1@0 1@1 2@2 \
-	hw 0
-tc qdisc replace dev ${ETH_1} parent ${HANDLE}:1 cbs \
-	idleslope 98688 sendslope -901312 hicredit 153 locredit -1389 \
-	offload 1
-tc qdisc replace dev ${ETH_1} parent ${HANDLE}:2 cbs \
-        idleslope 3648 sendslope -996352 hicredit 12 locredit -113 \
-        offload 1
-
-echo "${ETH_1} configured!\n"
+	echo "${ETH} configured!\n"
+done
 
 tc qdisc
