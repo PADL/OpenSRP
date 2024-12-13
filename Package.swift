c57efec3e98b6d3d56ab848e5f4fce6715e91a90
Luke Howard <lukeh@padl.com>
2024-12-14 07:06:56 +1100

    remove .git suffix from GitHub URLs

--- Package.swift
+++ Package.swift
@@ -101,7 +101,7 @@ let CommonPackageDependencies: [Package.Dependency] = [
   .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
   .package(url: "https://github.com/PADL/SocketAddress", from: "0.0.1"),
   .package(url: "https://github.com/lhoward/AsyncExtensions", from: "0.9.0"),
-  .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.3.0"),
+  .package(url: "https://github.com/swift-server/swift-service-lifecycle", from: "2.3.0"),
 ]
 
 let CommonProducts: [Product] = [
