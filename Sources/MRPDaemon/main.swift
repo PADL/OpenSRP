790b7911e8d6cd0a9cc235e7deae3d3f38d3275d
Luke Howard <lukeh@padl.com>
2025-07-30 11:26:59 +1000

    enableSRP need not imply MMRP, it is not used by 1722

--- Sources/MRPDaemon/main.swift
+++ Sources/MRPDaemon/main.swift
@@ -149,7 +149,6 @@ private final class MRPDaemon: AsyncParsableCommand {
       portExclusions: Set(excludeIface)
     )
     if enableSRP {
-      enableMMRP = false
       enableMVRP = true
       enableMSRP = true
     }
