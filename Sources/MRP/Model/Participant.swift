e5f6ba40e25aa54792876d29b8dec6d223eaa306
Luke Howard <lukeh@padl.com>
2025-08-29 09:05:01 +1000

    Revert "only create new attributes on New, JoinIn and JoinMt events"
    This reverts commit 841b215504b06b06cb55993d4db5466cf257c6e8.

--- Sources/MRP/Model/Participant.swift
+++ Sources/MRP/Model/Participant.swift
@@ -535,14 +535,12 @@ public final actor Participant<A: Application>: Equatable, Hashable, CustomStrin
       for i in 0..<Int(vectorAttribute.numberOfValues) {
         let attributeEvent = packedEvents[i]
         let attributeSubtype = vectorAttribute.applicationEvents?[i]
-        let createIfMissing = attributeEvent == .New || attributeEvent == .JoinIn ||
-          attributeEvent ==
-          .JoinMt
+
         guard let attribute = try? _findOrCreateAttribute(
           attributeType: message.attributeType,
           attributeSubtype: attributeSubtype,
           matching: .matchRelative((vectorAttribute.firstValue.value, UInt64(i))),
-          createIfMissing: createIfMissing
+          createIfMissing: true
         ) else { continue }
 
         if let attributeSubtype, attributeEvent == .JoinIn || attributeEvent == .JoinMt {
