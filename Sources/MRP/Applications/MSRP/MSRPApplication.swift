c7e2554d808d842db3c3d214f23d9b2cdf04872c
Luke Howard <lukeh@padl.com>
2025-12-06 09:08:49 +1100

    MSRP: fix fan-in port limit logic error
    The _isFanInPortLimitReached function had an inverted comparison operator that
    would incorrectly report the limit as reached when the count was at or below
    the maximum, rather than at or above it.

--- Sources/MRP/Applications/MSRP/MSRPApplication.swift
+++ Sources/MRP/Applications/MSRP/MSRPApplication.swift
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2024 PADL Software Pty Ltd
+// Copyright (c) 2024-2025 PADL Software Pty Ltd
 //
 // Licensed under the Apache License, Version 2.0 (the License);
 // you may not use this file except in compliance with the License.
@@ -624,7 +624,7 @@ extension MSRPApplication {
       }
     }
 
-    return fanInCount <= _maxFanInPorts
+    return fanInCount >= _maxFanInPorts
   }
 
   private func _compareStreamImportance(
